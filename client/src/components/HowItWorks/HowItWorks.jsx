9e7ddc10cba65360c8268498a91626805433c955
vasyl <vasia.kit434@gmail.com>
2024-12-07 23:50:05 +0200

    Add new element to HowItWorksPage

--- client/src/components/HowItWorks/HowItWorks.jsx
+++ client/src/components/HowItWorks/HowItWorks.jsx
@@ -1,36 +1,136 @@
 import React, { Component } from 'react';
+import { LiaLongArrowAltRightSolid } from 'react-icons/lia';
 import styles from './HowItWorks.module.sass';
 class HowItWorks extends Component {
   render () {
     return (
-      <section>
-        <div className={styles.hhInner}>
-          <div className={styles.content}>
-            <div className={styles.container}>
-              <div className={styles.flex}>
-                <div className={styles.left}>
-                  <h4>World's #1 Naming Platform</h4>
-                  <h1>How Does Atom Work?</h1>
-                  <p>
-                    Atom helps you come up with a great name for your business
-                    by combining the power of crowdsourcing with sophisticated
-                    technology and Agency-level validation services.
-                  </p>
-                </div>
-                <div className={styles.right}>
-                  <div className={styles.videoBox}>
-                    <iframe
-                      src='https://iframe.mediadelivery.net/embed/239474/327efcdd-b1a2-4891-b274-974787ae8362?autoplay=false&amp;loop=false&amp;muted=false&amp;preload=true&amp;responsive=true'
-                      allow='accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;'
-                      allowfullscreen='true'
-                    ></iframe>
+      <>
+        <section>
+          <div className={styles.hhInner}>
+            <div className={styles.content}>
+              <div className={styles.container}>
+                <div className={styles.flex}>
+                  <div className={styles.left}>
+                    <h4>World's #1 Naming Platform</h4>
+                    <h1>How Does Atom Work?</h1>
+                    <p>
+                      Atom helps you come up with a great name for your business
+                      by combining the power of crowdsourcing with sophisticated
+                      technology and Agency-level validation services.
+                    </p>
+                  </div>
+                  <div className={styles.right}>
+                    <div className={styles.videoBox}>
+                      <iframe
+                        src='https://iframe.mediadelivery.net/embed/239474/327efcdd-b1a2-4891-b274-974787ae8362?autoplay=false&amp;loop=false&amp;muted=false&amp;preload=true&amp;responsive=true'
+                        allow='accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;'
+                        allowFullScreen={true}
+                      ></iframe>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+
+        <section className={styles.homeBlock}>
+          <div className={styles.container}>
+            <div className={styles.hbCaption}>
+              <div className={styles.max500}>
+                <span>Our Services</span>
+                <h2>3 Ways To Use Atom</h2>
+                <p>
+                  Atom offers 3 ways to get you a perfect name for your
+                  business.
+                </p>
+              </div>
+            </div>
+            <div className={styles.row}>
+              <div className={styles.colCard}>
+                <div className={`${styles.item} ${styles.alignBox}`}>
+                  <div>
+                    <div className={styles.icon}>
+                      <img alt='' src='/staticImages/g1.svg' />
+                    </div>
+                    <h3>Launch a Contest</h3>
+                    <p className={styles.mb5}>
+                      Work with hundreds of creative experts to get custom name
+                      suggestions for your business or brand. All names are
+                      auto-checked for URL availability.
+                    </p>
+                  </div>
+                  <div className={styles.mb3}>
+                    <a
+                      href='/login'
+                      className={`${styles.button} ${styles.buttonBrand}`}
+                    >
+                      <span>Launch a Contest</span>{' '}
+                      <LiaLongArrowAltRightSolid
+                        className={styles.arrowRight}
+                      />
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.colCard}>
+                <div className={`${styles.item} ${styles.alignBox}`}>
+                  <div>
+                    <div className={styles.icon}>
+                      <img alt='' src='/staticImages/g2.svg' />
+                    </div>
+                    <h3>Explore Names For Sale</h3>
+                    <p className={styles.mb5}>
+                      Our branding team has curated thousands of pre-made names
+                      that you can purchase instantly. All names include a
+                      matching URL and a complimentary Logo Design
+                    </p>
+                  </div>
+                  <div className={styles.mb3}>
+                    <a
+                      href='/login'
+                      className={`${styles.button} ${styles.buttonBrand}`}
+                    >
+                      <span>Explore Names For Sale</span>{' '}
+                      <LiaLongArrowAltRightSolid
+                        className={styles.arrowRight}
+                      />
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.colCard}>
+                <div className={`${styles.item} ${styles.alignBox}`}>
+                  <div>
+                    <div className={styles.icon}>
+                      <img alt='' src='/staticImages/g3.svg' />
+                    </div>
+                    <h3>Agency-level Managed Contests</h3>
+                    <p className={styles.mb5}>
+                      Our Managed contests combine the power of crowdsourcing
+                      with the rich experience of our branding consultants. Get
+                      a complete agency-level experience at a fraction of Agency
+                      costs
+                    </p>
+                  </div>
+                  <div className={styles.mb3}>
+                    <a
+                      href='/login'
+                      className={`${styles.button} ${styles.buttonBrand}`}
+                    >
+                      <span>Learn More</span>{' '}
+                      <LiaLongArrowAltRightSolid
+                        className={styles.arrowRight}
+                      />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.textCenter}></div>
+          </div>
+        </section>
+      </>
     );
   }
 }
