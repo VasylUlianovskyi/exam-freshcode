fd2fb3d5f2c9bfd085298296c68fc912194c58a9
vasyl <vasia.kit434@gmail.com>
2025-03-18 21:33:27 +0200

    Fix change avatar issues

--- client/src/constants.js
+++ client/src/constants.js
@@ -21,7 +21,7 @@ export default {
   publicURL:
     env === 'production'
       ? `http://${serverIP}:80/images/`
-      : `http://${serverIP}:${serverPort}/images/`,
+      : `http://${serverIP}:${serverPort}`,
   NORMAL_PREVIEW_CHAT_MODE: 'NORMAL_PREVIEW_CHAT_MODE',
   FAVORITE_PREVIEW_CHAT_MODE: 'FAVORITE_PREVIEW_CHAT_MODE',
   BLOCKED_PREVIEW_CHAT_MODE: 'BLOCKED_PREVIEW_CHAT_MODE',
