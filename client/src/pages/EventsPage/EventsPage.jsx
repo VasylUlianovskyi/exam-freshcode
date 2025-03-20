07dc747f8e3eb2d33cfbbffd717e604bf97cf037
vasyl <vasia.kit434@gmail.com>
2025-03-20 22:39:09 +0200

    Moved localStorage operations from reducers to useEffect

--- client/src/pages/EventsPage/EventsPage.jsx
+++ client/src/pages/EventsPage/EventsPage.jsx
@@ -9,20 +9,32 @@ import Footer from '../../components/Footer/Footer';
 import styles from './EventsPage.module.sass';
 import EventForm from './../../components/Timers/EventForm/EventForm';
 import EventList from './../../components/Timers/EventsList/EventsList';
-import { updateTimers } from '../../store/slices/timersSlice';
+import { updateTimers, setTimers } from '../../store/slices/timersSlice';
+
+const LOCAL_STORAGE_KEY = 'timers';
 
 const EventsPage = () => {
   const dispatch = useDispatch();
   const user = useSelector(state => state.userStore?.data);
+  const events = useSelector(state => state.timers.events);
+
+  useEffect(() => {
+    const storedTimers = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (storedTimers) {
+      dispatch(setTimers(JSON.parse(storedTimers)));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(events));
+  }, [events]);
 
   useEffect(() => {
     const interval = setInterval(() => {
       dispatch(updateTimers());
     }, 1000);
 
-    return () => {
-      clearInterval(interval);
-    };
+    return () => clearInterval(interval);
   }, [dispatch]);
 
   if (!user || user.role !== CONSTANTS.CUSTOMER) {
--- client/src/store/slices/timersSlice.js
+++ client/src/store/slices/timersSlice.js
@@ -7,26 +7,24 @@ const loadTimersFromStorage = () => {
   return storedTimers ? JSON.parse(storedTimers) : [];
 };
 
-const saveTimersToStorage = timers => {
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(timers));
-};
-
 const timersSlice = createSlice({
   name: 'timers',
   initialState: {
-    events: loadTimersFromStorage(),
+    events: [],
     activeAlerts: 0,
   },
   reducers: {
+    setTimers: (state, action) => {
+      state.events = action.payload;
+      state.activeAlerts = calculateActiveAlerts(state.events);
+    },
     addTimer: (state, action) => {
       state.events.push(action.payload);
       state.events.sort((a, b) => new Date(a.date) - new Date(b.date));
-      saveTimersToStorage(state.events);
       state.activeAlerts = calculateActiveAlerts(state.events);
     },
     removeTimer: (state, action) => {
       state.events = state.events.filter(event => event.id !== action.payload);
-      saveTimersToStorage(state.events);
       state.activeAlerts = calculateActiveAlerts(state.events);
     },
     updateTimers: state => {
@@ -34,15 +32,12 @@ const timersSlice = createSlice({
 
       state.events = state.events.map(event => {
         const eventTime = new Date(event.date);
-
         if (eventTime <= now) {
           return { ...event, status: 'expired' };
         }
-
         return event;
       });
 
-      saveTimersToStorage(state.events);
       state.activeAlerts = calculateActiveAlerts(state.events);
     },
   },
@@ -59,5 +54,6 @@ const calculateActiveAlerts = events => {
   }).length;
 };
 
-export const { addTimer, removeTimer, updateTimers } = timersSlice.actions;
+export const { setTimers, addTimer, removeTimer, updateTimers } =
+  timersSlice.actions;
 export default timersSlice.reducer;
