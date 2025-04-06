46a63dfcd60a59b2bc2162ba3c4bcb09312c3f94
vasyl <vasia.kit434@gmail.com>
2025-04-06 16:35:53 +0300

    Update FAQ sections with rich content rendering and styles

--- client/src/components/HowItWorks/HowItWorks.jsx
+++ client/src/components/HowItWorks/HowItWorks.jsx
@@ -21,23 +21,56 @@ const faqs = [
   },
   {
     question: 'What if I do not like any submissions?',
-    answer: `While it is unusually rare that you will not like any names provided, we have a few options in case this problem occurs:
-
-    If the contest ends and you have not yet found a name that you’d like to move forward with, we can provide complimentary extension of your contest as well as a complimentary consultation with one of our branding consultants (a $99 value).
-    By exploring our premium domain marketplace you can apply the contest award towards the purchase of any name listed for sale.
-    If you choose the Gold package or Platinum package and keep the contest as 'Not Guaranteed', you can request a partial refund if you choose not to move forward with any name from you project. (Please note that the refund is for the contest award). Here is a link to our Refund Policy
-`,
+    content: (
+      <>
+        <p>
+          While it is unusually rare that you will not like any names provided,
+          we have a few options in case this problem occurs:
+        </p>
+        <ul>
+          <li>
+            If the contest ends and you have not yet found a name that you’d
+            like to move forward with, we can provide complimentary extension of
+            your contest as well as a complimentary consultation with one of our
+            branding consultants (a $99 value).
+          </li>
+          <li>
+            By exploring our premium domain marketplace you can apply the
+            contest award towards the purchase of any name listed for sale.
+          </li>
+          <li>
+            If you choose the Gold package or Platinum package and keep the
+            contest as <i>Not Guaranteed</i>, you can request a partial refund
+            if you choose not to move forward with any name from your project.
+            (Please note that the refund is for the contest award). Here is a
+            link to our <a href='#'>Refund Policy</a>.
+          </li>
+        </ul>
+      </>
+    ),
   },
   {
     question: 'How much does it cost?',
-    answer:
-      'Our naming competitions start at $299, and our logo design competitions start at $299. Also, there are three additional contest level that each offer more features and benefits. See our Pricing Page for details',
+    content: (
+      <p>
+        Our naming competitions start at $299, and our logo design competitions
+        start at $299. Also, there are three additional contest level that each
+        offer more features and benefits. See our <a href='#'>Pricing Page</a>{' '}
+        for details.
+      </p>
+    ),
   },
   {
     question:
       'I need both a Name and a Logo. Do you offer any discount for multiple contests?',
-    answer:
-      'Our naming competitions start at $299, and our logo design competitions start at $299. Also, there are three additional contest level that each offer more features and benefits. See our Pricing Page for details',
+    content: (
+      <p>
+        Yes! We have many contest bundles – our most popular being our Name,
+        Tagline, and Logo bundle. Bundles allow you to purchase multiple
+        contests at one time and save as much as from $75 – $400. You can learn
+        more about our bundle options on our <a href='#'>Pricing Page</a>.
+      </p>
+    ),
   },
   {
     question: 'What if I want to keep my business idea private?',
@@ -46,18 +79,34 @@ const faqs = [
   },
   {
     question: 'Can you serve customers outside the US?',
-    answer: `Absolutely. Atom services organizations across the globe. Our customer come from many countries, such as the United States, Australia, Canada, Europe, India, and MENA. We've helped more than 25,000 customer around the world.`,
+    answer:
+      "Absolutely. Atom services organizations across the globe. Our customer come from many countries, such as the United States, Australia, Canada, Europe, India, and MENA. We've helped more than 25,000 customers around the world.",
   },
   {
     question: 'Can I see any examples?',
-    answer: `Our creatives have submitted more than 6 Million names and thousands of logos on our platform. Here are some examples of Names, Taglines, and Logos that were submitted in recent contests.
-
-      Name Examples
-      Tagline Examples
-      Logo Examples
-`,
+    content: (
+      <>
+        <p>
+          Our creatives have submitted more than 6 Million names and thousands
+          of logos on our platform. Here are some examples of Names, Taglines,
+          and Logos that were submitted in recent contests.
+        </p>
+        <ul>
+          <li>
+            <a href='#'>Name Examples</a>
+          </li>
+          <li>
+            <a href='#'>Tagline Examples</a>
+          </li>
+          <li>
+            <a href='#'>Logo Examples</a>
+          </li>
+        </ul>
+      </>
+    ),
   },
 ];
+
 const marketPlaceFaqs = [
   {
     question: `What's included with a Domain Purchase?`,
@@ -76,51 +125,142 @@ const marketPlaceFaqs = [
       'We offer payment plans for many domains in our Marketplace. If you purchase a domain on a payment plan, we hold the domain in an Escrow account until it is fully paid off. However our team can assist you with making any changes to the domains (such as Nameserver changes), so that you can start using the domain right away after making your first installment payment.',
   },
 ];
+
 const manageFaqs = [
   {
     question: 'What are Managed Contests?',
-    answer: `The 'Managed' option is a fully managed service by Atom Branding experts. It includes a formal brief preparation by Atom team and management of your contest. Managed Contests are a great fit for companies that are looking for an 'Agency' like experience and they do not want to manage the contest directly.
-Our branding team has directly managed hundreds of branding projects and has learned several best practices that lead to successful project outcomes. Our team will apply all best practices towards the management of your branding project.
-Learn more about our Managed Contest Service`,
+    content: (
+      <>
+        <p>
+          The <strong>'Managed'</strong> option is a fully managed service by
+          Atom Branding experts. It includes a formal brief preparation by Atom
+          team and management of your contest. Managed Contests are a great fit
+          for companies that are looking for an <strong>'Agency'</strong>-like
+          experience and they do not want to manage the contest directly.
+        </p>
+        <p>
+          Our branding team has directly managed hundreds of branding projects
+          and has learned several best practices that lead to successful project
+          outcomes. Our team will apply all best practices towards the
+          management of your branding project.
+        </p>
+        <p>
+          Learn more about our <a href='#'>Managed Contest Service</a>
+        </p>
+      </>
+    ),
   },
   {
     question: `What's a typical timeline for a Managed Contest?`,
-    answer: `The overall process takes 12-13 days.
-
-The Managed projects start with a project kick-off call with your Branding Consultant. You can schedule this call online immediately after making your payment.
-After your kick-off call, the Branding consultant will write your project brief and send for your approval within 1 business day.
-Upon your approval, the contest will go live. The branding consultant will help manage your project throughout the brainstorming phase (typically 5 days).
-Upon the completion of brainstorming phase, the branding consultant will work with you to test the top 6 names from your Shortlist (3-5 Days). In addition, the branding consultant will coordinate the detailed Trademark screening (1-3 days)`,
+    content: (
+      <>
+        <p>
+          <strong>The overall process takes 12–13 days.</strong>
+        </p>
+        <ul>
+          <li>
+            The Managed projects start with a project kick-off call with your
+            Branding Consultant. You can schedule this call online immediately
+            after making your payment.
+          </li>
+          <li>
+            After your kick-off call, the Branding consultant will write your
+            project brief and send for your approval within 1 business day.
+          </li>
+          <li>
+            Upon your approval, the contest will go live. The branding
+            consultant will help manage your project throughout the
+            brainstorming phase (typically 5 days).
+          </li>
+          <li>
+            Upon the completion of brainstorming phase, the branding consultant
+            will work with you to test the top 6 names from your Shortlist (3–5
+            Days). In addition, the branding consultant will coordinate the
+            detailed Trademark screening (1–3 days).
+          </li>
+        </ul>
+      </>
+    ),
   },
   {
     question: 'How much do Managed Contests cost?',
-    answer: `We offer two levels of Managed Contests. Standard ($1499) and Enterprise ($2999). The Enterprise managed contest includes:
-
-(1) a $500 award amount (instead of $300), which will attract our top Creatives and provide more options to choose from;
-(2) we will ensure a senior member of our branding team is assigned to your project and the branding team will invest about 3X more time in the day-to-day management of your project;
-(3) you will receive more high-end trademark report and 5X more responses for your audience test.
-Here is a link to our Pricing page with a detailed comparison of the two packages.
-`,
+    content: (
+      <>
+        <p>
+          We offer two levels of Managed Contests. Standard ($1499) and
+          Enterprise ($2999). The Enterprise managed contest includes:
+        </p>
+        <ul>
+          <li>
+            A $500 award amount (instead of $300), which will attract our top
+            Creatives and provide more options to choose from.
+          </li>
+          <li>
+            We will ensure a senior member of our branding team is assigned to
+            your project and the branding team will invest about 3X more time in
+            the day-to-day management of your project.
+          </li>
+          <li>
+            You will receive more high-end trademark report and 5X more
+            responses for your audience test.
+          </li>
+        </ul>
+        <p>
+          Here is a link to our <a href='#'>Pricing page</a> with a detailed
+          comparison of the two packages.
+        </p>
+      </>
+    ),
   },
   {
     question: 'Where are the Branding Consultants located?',
-    answer:
-      'All our branding consultants are based in in our Headquarters (Hoffman Estates, IL). Our branding consultants have many years of experience in managing hundreds of branding projects for companies ranging from early stage startups to Fortune 500 corporations.',
+    content: (
+      <p>
+        All our branding consultants are based in our Headquarters (Hoffman
+        Estates, IL). Our branding consultants have many years of experience in
+        managing hundreds of branding projects for companies ranging from early
+        stage startups to Fortune 500 corporations.
+      </p>
+    ),
   },
 ];
+
 const creativeFaqs = [
   {
     question: `Can anyone join your platform?`,
-    answer: `We are open to anyone to signup. However, we have an extensive 'Quality Scoring' process which ensures that high quality creatives have the ability to continue to participate in the platform. On the other hand, we limit the participation from those creatives who do not consistently receive high ratings.`,
+    content: (
+      <p>
+        We are open to anyone to signup. However, we have an extensive{' '}
+        <a href='#'>‘Quality Scoring’</a> process which ensures that high
+        quality creatives have the ability to continue to participate in the
+        platform. On the other hand, we limit the participation from those
+        creatives who do not consistently receive high ratings.
+      </p>
+    ),
   },
   {
     question: `Can I start participating immediately upon signing up?`,
-    answer: `When you initially signup, you are assigned few contests to assess your overall quality of submissions. Based upon the quality of your submissions, you will continue to be assigned additional contests. Once you have received enough high ratings on your submissions, your account will be upgraded to 'Full Access', so that you can begin participating in all open contests.`,
+    content: (
+      <p>
+        When you initially signup, you are assigned few contests to assess your
+        overall quality of submissions. Based upon the quality of your
+        submissions, you will continue to be assigned additional contests. Once
+        you have received enough high ratings on your submissions, your account
+        will be upgraded to <strong>‘Full Access’</strong>, so that you can
+        begin participating in all open contests.
+      </p>
+    ),
   },
   {
     question: 'How Do I Get Paid?',
-    answer:
-      'We handle creative payouts via Paypal or Payoneer. Depending upon your country of residence, we may require additional documentation to verify your identity as well as your Tax status.',
+    content: (
+      <p>
+        We handle creative payouts via <strong>Paypal</strong> or{' '}
+        <strong>Payoneer</strong>. Depending upon your country of residence, we
+        may require additional documentation to verify your identity as well as
+        your Tax status.
+      </p>
+    ),
   },
 ];
 
@@ -187,7 +327,7 @@ class HowItWorks extends Component {
           </div>
 
           <div
-            className={styles.answerWrapper}
+            className={styles.answer}
             style={{
               maxHeight: isOpen
                 ? this.answerRefs[uniqueId]?.scrollHeight + 'px'
@@ -196,12 +336,12 @@ class HowItWorks extends Component {
               transition: 'max-height 0.5s ease',
             }}
           >
-            <p
+            <div
               className={styles.answer}
               ref={el => (this.answerRefs[uniqueId] = el)}
             >
-              {faq.answer}
-            </p>
+              {faq.content ? faq.content : <p>{faq.answer}</p>}
+            </div>
           </div>
         </div>
       );
